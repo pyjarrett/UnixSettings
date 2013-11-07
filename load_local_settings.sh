77bfaf5581f51f1d7062ef0a17baf04c512e587f
Paul Jarrett <jarrett.paul.young@gmail.com>
2013-11-07 07:03:04 -0500

    Fixed to put uploaded settings into profile_customization.

--- load_local_settings.sh
+++ load_local_settings.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Loads local settings into this directory
 #
-cp ~/.bash_profile .
-cp ~/.vimrc .
-cp ~/.screenrc .
+cp ~/.bash_profile profile_customization/
+cp ~/.vimrc profile_customization/
+cp ~/.screenrc profile_customization/
