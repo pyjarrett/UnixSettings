9bd893345520661499d5b150a8c556dabc518cfd
Paul Jarrett <jarrett.paul.young@gmail.com>
2017-04-10 20:11:24 -0400

    Fixes profile installation.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ INSTALL_DIR=`echo ~`
 echo "Loading profile_customization into $INSTALL_DIR"
 echo "Installing the following customization files:"
 ls -1A profile_customization
-cp -R profile_customization/ ~
+cp -R profile_customization ~
 
 # Reload the new configuration into bash
 echo
